2c6f6831cab6a18e42603a585effcd81b134b118
andreluza <luza.andre@gmail.com>
2023-03-07 09:42:33 -0300

    updating code

--- R/script.R
+++ R/script.R
@@ -12,7 +12,6 @@ require(jagsUI)
 require(coda)
 require(here)
 
-
 # load data Andre
 data_damself <- read_excel(here ("Data",'STEFUS_data_ALLuza.xlsx'),trim_ws = TRUE)  
 
@@ -27,7 +26,7 @@ data_damself_thomas <- read_excel(here ("Data",'STEFUS_data_TBanha.xlsx'),trim_w
   mutate (individual = max(data_damself_renato$individual)+individual)
 
 # load data Carol
-data_damself_carol <- read_excel(here ("Data",'STEFUS_data_TBanha.xlsx'),trim_ws = TRUE)  %>%
+data_damself_carol <- read_excel(here ("Data",'STEFUS_data_CSchlosser.xlsx'),trim_ws = TRUE)  %>%
   mutate (sequence_obs = as.numeric(sequence_obs)) %>%
   mutate (individual = max(data_damself_thomas$individual)+individual)
 
@@ -37,6 +36,10 @@ data_damself<- rbind (data_damself,
                       data_damself_thomas,
                       data_damself_carol)
 
+# subadult
+data_damself <- data_damself %>%
+  mutate (age = recode (age, 
+                        "intermediary" = "subadult"))
 
 # arrange data for state-space model
 data_damself_shifts <- data_damself %>% 
@@ -69,13 +72,13 @@ data_damself_shifts<-data_damself_shifts%>%
 # transition matrix
 require(reshape)
 # bites
-wide_df_bites <- cast (formula = site+individual ~ Count,
+wide_df_bites <- cast (formula = age+individual ~ Count,
                        value="code_beh_bite",
                        fill = NA,
                        fun.aggregate = sum,  
                        data = data_damself_shifts)
 # chase
-wide_df_chase <- cast (formula = site+individual ~ Count,
+wide_df_chase <- cast (formula = age+individual ~ Count,
                        value="code_beh_chase",
                        fill = NA,
                        fun.aggregate = sum,  
@@ -134,8 +137,8 @@ mcmc_res <- lapply (list (wide_df_bites,
     str(jags.data <- list(y = i[,-c(1,2)], 
                           nind = nrow(i), 
                           n_seq= ncol(i[,-c(1,2)]),
-                          site=as.numeric(as.factor (i$site)),
-                          nsite=length(unique(as.numeric(as.factor (i$site))))))
+                          site=as.numeric(as.factor (i$age)), # i$site
+                          nsite=length(unique(as.numeric(as.factor (i$age)))))) # i$site
     
     # run MCMC
     samples_MCMC  <- jags(data = jags.data,
@@ -155,9 +158,9 @@ mcmc_res <- lapply (list (wide_df_bites,
     
     # results per site
     df_plot_site <- data.frame (
-      samples_MCMC$summary[1:6,],
-      State = c(rep("Shift",3),rep ("Keep",3)),
-      Site = c("Bare", "Cabe", "Seg","Bare", "Cabe", "Seg")
+      samples_MCMC$summary[1:4,],# [1:6,],
+      State = c(rep("Shift",2),rep ("Keep",2)),#c(rep("Shift",3),rep ("Keep",3)),
+      Site = c("adult", "subadult", "adult", "subadult")#c("Bare", "Cabe", "Seg","Bare", "Cabe", "Seg")
     )
     
     
@@ -215,7 +218,7 @@ a <- ggplot (mcmc_res_site,
            label="Chase->Bite",
            color="black")+
   annotate(geom="text",x=2.1,
-           y=0.37,
+           y=0.25,
            label="Bite->Chase",
            color="black")+
 annotate(geom="text",x=0.87,
@@ -230,14 +233,145 @@ annotate(geom="text",x=0.87,
 #pdf (here ("output", "rates.pdf"))
 a
 #dev.off()
-
 # averages
-
 mcmc_res_av <- sapply (mcmc_res, "[", "df_plot")
 names(mcmc_res_av) <- c("Bite", "Chase")
 mcmc_res_av <- do.call(rbind, mcmc_res_av)
 mcmc_res_av$Behavior <- sapply (strsplit (rownames(mcmc_res_av),"\\."), "[[",1)
 mcmc_res_av[,c(1,3,7)]
+ 
+
+# apply the model to sites
+
+# transition matrix
+require(reshape)
+# bites
+wide_df_bites <- cast (formula = site+individual ~ Count,
+                       value="code_beh_bite",
+                       fill = NA,
+                       fun.aggregate = sum,  
+                       data = data_damself_shifts)
+# chase
+wide_df_chase <- cast (formula = site+individual ~ Count,
+                       value="code_beh_chase",
+                       fill = NA,
+                       fun.aggregate = sum,  
+                       data = data_damself_shifts)
+
+
+
+
+# apply test for the two datasets
+mcmc_res <- lapply (list (wide_df_bites,
+                          wide_df_chase), function (i) {
+                            
+                            ## bundle data
+                            str(jags.data <- list(y = i[,-c(1,2)], 
+                                                  nind = nrow(i), 
+                                                  n_seq= ncol(i[,-c(1,2)]),
+                                                  site=as.numeric(as.factor (i$site)), # 
+                                                  nsite=length(unique(as.numeric(as.factor (i$site)))))) # i$site
+                            
+                            # run MCMC
+                            samples_MCMC  <- jags(data = jags.data,
+                                                  parameters.to.save=params, 
+                                                  model = "Model_dyn.txt", 
+                                                  inits = NULL,
+                                                  n.chains = nc, 
+                                                  n.thin = nt, 
+                                                  n.iter = ni, 
+                                                  n.burnin = nb)
+                            
+                            # data to plot
+                            df_plot <- data.frame (
+                              samples_MCMC$summary[(grep ("av",rownames(samples_MCMC$summary))),],
+                              State = c("Shift", "Keep")
+                            )
+                            
+                            # results per site
+                            df_plot_site <- data.frame (
+                              samples_MCMC$summary[1:6,],
+                              State = c(rep("Shift",3),rep ("Keep",3)),
+                              Site = c("Bare", "Cabe", "Seg","Bare", "Cabe", "Seg")
+                            )
+                            
+                            
+                            res <- list (df_plot=df_plot,
+                                         df_plot_site=df_plot_site)
+                            res
+                            
+                          })
+
+mcmc_res_site <- sapply (mcmc_res, "[", "df_plot_site")
+names(mcmc_res_site) <- c("Bite", "Chase")
+mcmc_res_site <- do.call(rbind, mcmc_res_site)
+mcmc_res_site$Behavior <- sapply (strsplit (rownames(mcmc_res_site),"\\."), "[[",1)
+
+# plot
+require(ggplot2)
+dodge <- c(0.4,0.4)
+pd <- position_dodge(dodge)
+pdf_pt <- position_dodge(dodge)
+# plot
+a_sites <- ggplot (mcmc_res_site, 
+             
+             aes  (y=mean, x=State, 
+                   fill=Behavior,colour=Site)) +
+  facet_grid(~Site)+
+  
+  geom_errorbar(aes(ymin=X2.5.,ymax=X97.5.),width = 0.2,size=1,
+                position=pd)  + 
+  
+  scale_colour_viridis_d(option = "magma",begin = 0.1,end=0.7)+
+  
+  theme_classic() + 
+  
+  geom_hline(yintercept=0.5, linetype="dashed", 
+             color = "gray50", size=0.5)+
+  
+  geom_vline(xintercept=1.5, linetype="dashed", 
+             color = "gray50", size=0.5)+
+  
+  geom_point(position=(pdf_pt), 
+             size=3.5)+ 
+  
+  xlab("State") + 
+  
+  ylab ("Probability of transition") + 
+  
+  #xlim(-0.5,0.5) +
+  
+  theme(axis.text = element_text(angle = 0,size=12),
+        axis.title = element_text(angle = 0,size=14),
+        legend.position = "none") + 
+  
+  annotate(geom="text",x=2,
+           y=0.62,
+           label="Chase->Bite",
+           color="black")+
+  annotate(geom="text",x=2.1,
+           y=0.25,
+           label="Bite->Chase",
+           color="black")+
+  annotate(geom="text",x=0.87,
+           y=0.76,
+           label="Bite",
+           color="black")+
+  annotate(geom="text",x=1.1,
+           y=0.57,
+           label="Chase",
+           color="black")
+#save
+#pdf (here ("output", "rates.pdf"))
+a_sites
+#dev.off()
+
+# averages
+mcmc_res_av_sites <- sapply (mcmc_res, "[", "df_plot")
+names(mcmc_res_av_sites) <- c("Bite", "Chase")
+mcmc_res_av_sites <- do.call(rbind, mcmc_res_av_sites)
+mcmc_res_av_sites$Behavior <- sapply (strsplit (rownames(mcmc_res_av_sites),"\\."), "[[",1)
+mcmc_res_av_sites[,c(1,3,7)]
 
 # ------------------------------
 
@@ -269,7 +403,7 @@ summary_stats <- data_damself %>%
                                    "intraspecific_chase"=0,
                                    "both_chase"=0)) %>%
   group_by (site,individual,variable,age) %>%
-  summarize(sum_count = sum (count)) %>%
+  summarize(sum_count = sum (count)/3) %>%
   #group_by (site,variable,age) %>%
   #summarise(av = mean (sum_count),
   #          sd=sd(sum_count))
@@ -279,9 +413,54 @@ summary_stats <- data_damself %>%
   theme_bw()+
   facet_wrap(~site,nrow=3)+
   xlab ("Behavior")+
-  ylab ("Count")+
+  ylab ("Rate per minute")+
   theme(axis.text.x=element_text(angle=25,vjust=0.5),
-        legend.position = c(0.8,0.9))
+        legend.position = c(0.8,0.8))
+
+
+# anova
+data_anova_bites <- data_damself %>% 
+  filter (variable != "NA") %>%
+  #filter (variable != "both_chase") %>%
+  mutate (coded_behavior = recode (variable, 
+                                   "bites" = "bites",
+                                   "interspecific_chase"="chase",
+                                   "intraspecific_chase"="chase",
+                                   "both_chase"="chase")) %>%
+  group_by (site,individual,coded_behavior,age) %>%
+  summarize(sum_count = sum (count)/3) %>%
+  filter (coded_behavior == "bites")
+
+# no effect of site and age on bite rates 
+summary(aov (formula = sum_count~ age*site,
+       data=data_anova_bites))
+
+# chases
+data_anova_chase <- data_damself %>% 
+  filter (variable != "NA") %>%
+  #filter (variable != "both_chase") %>%
+  mutate (coded_behavior = recode (variable, 
+                                   "bites" = "bites",
+                                   "interspecific_chase"="chase",
+                                   "intraspecific_chase"="chase",
+                                   "both_chase"="chase",
+                                   "total_chasing" = "chase")) %>%
+  group_by (site,individual,coded_behavior,age) %>%
+  summarize(sum_count = sum (count)/3) %>%
+  filter (coded_behavior == "chase")
+
+# no effect of site and age on bite rates 
+summary(aov (formula = sum_count~ age*site,
+             data=data_anova_chase))
+
+# plot
+plot_anova <- ggplot (data_anova_chase, aes (x=age, y=sum_count/3))+
+  geom_jitter()+
+  geom_boxplot (fill= "lightgreen",alpha=0.6)+
+  theme_bw()+xlab ("Age")+ylab ("# chases per minute")+
+  theme (axis.text = element_text(size=12),
+         axis.title = element_text(size=15))
+  
 
 
 # forraging vs percution rate
@@ -291,9 +470,10 @@ test_rel<- data_damself %>%
                          "bites" = 1,
                          "interspecific_chase"=0,
                          "intraspecific_chase"=0,
+                         "total_chasing"=0,
                          "both_chase"=0)) %>%
   group_by (individual, coded_behavior,age) %>%
-  summarise(ct = sum (count)) %>%
+  summarise(ct = sum (count)/3) %>%
   cast(individual+age~coded_behavior, 
        value="ct",
        function.aggregate = sum)
@@ -308,22 +488,22 @@ plot_cor <- ggplot (test_rel %>%
   scale_colour_viridis_d(option="magma",end=0.5)+
   geom_point ()+
   geom_smooth(method = "lm") + 
-  xlab ("Count of chases") + 
-  ylab ("Count of bites")+
+  xlab ("Chase rate per minute") + 
+  ylab ("Bite rate per minute")+
   theme_bw() +
   theme (axis.title = element_text(size=14),
          axis.text = element_text(size=12),
          legend.position = c(0.85,0.9)) + 
   annotate(geom="text",
-           x=16,
-           y=11,
-           label="rho=-0.34,ns",
+           x=5,
+           y=6,
+           label="rho=0.05,ns",
            size=5,
            color="#B3005E")+
   annotate(geom="text",
-           x=10,
-           y=15,
-           label="rho=0.12,ns",
+           x=5,
+           y=4,
+           label="rho=0.04,ns",
            size=5,
            color="black")
 
@@ -331,17 +511,17 @@ plot_cor <- ggplot (test_rel %>%
 cor.test (test_rel$chase,test_rel$bit) #overall relationship
 cor.test (test_rel$chase[which(test_rel$age == "adult")],
           test_rel$bit[which(test_rel$age == "adult")])
-cor.test (test_rel$chase[which(test_rel$age == "intermediary")],
-          test_rel$bit[which(test_rel$age == "intermediary")])
+cor.test (test_rel$chase[which(test_rel$age == "subadult")],
+          test_rel$bit[which(test_rel$age == "subadult")])
 
 
 # chased spp
-chased_spp<- lapply (unique(data_damself$site), function (i)
+chased_spp<- lapply (unique(data_damself$age), function (i) #i$site
 
   data.frame (count=table(gsub (" ","", gsub('[0-9]+', '', 
-                           unlist(strsplit (data_damself$species_chased[which(data_damself$site == i)], ","))))))
+                           unlist(strsplit (data_damself$species_chased[which(data_damself$age == i)], ","))))))
 )
-names(chased_spp) <- unique(data_damself$site)
+names(chased_spp) <- unique(data_damself$age)
 chased_spp <- do.call(rbind,chased_spp)
 chased_spp$site <- gsub('[0-9]+', '', rownames(chased_spp))
 
@@ -353,5 +533,7 @@ plot_freq_chased <- ggplot(chased_spp%>%
   scale_fill_viridis_d(option="magma",direction=-1,end=0.9)+
   ylab ("Frequency (# chases)") + 
   xlab ("Site")+
-  theme(legend.title = element_blank())
+  theme(legend.title = element_blank(),
+        axis.text = element_text(size=12),
+               axis.title = element_text(size=15))
 
